a4d67fe7c405cc75db662f633d9170bc132e1e37
Kamil Svoboda <kamil.svoboda@tmapy.cz>
2016-03-16 14:39:00 +0100

    enhandec logging

--- TRexServer/Controllers/StatusController.cs
+++ TRexServer/Controllers/StatusController.cs
@@ -15,22 +15,34 @@ namespace TRexServer.Controllers
         // GET api/status
         public void Get(StatusDTO value)
         {
-            Logger.Debug("i: " + value.i + ", a: " + value.a + ", o: " + value.o + ", l: " + value.l + ", s: " + value.s + ", b: " + value.b + ", g: " + value.g + ", t: " + value.t?.ToString("yyyy-MM-dd'T'HH:mm:ss") + ", k: " + value.k);
+            string logText = "i: " + value.i + ", a: " + value.a + ", o: " + value.o + ", l: " + value.l + ", s: " +
+                             value.s + ", b: " + value.b + ", g: " + value.g + ", t: " +
+                             value.t?.ToString("yyyy-MM-dd'T'HH:mm:ss") + ", k: " + value.k;
             if (SecurityHelper.CheckAccessKey(value))
             {
                 var dtoFactory = new DtoFactory();
                 dtoFactory.InsertOrUpdateDatabase(value);
+            }
+            else
+            {
+                Logger.Warn("Pozice odmítnuta: " + logText);
             }
         }
 
         // POST api/status
         public void Post([FromBody] StatusDTO value)
         {
-            Logger.Debug("i: " + value.i + ", a: " + value.a + ", o: " + value.o + ", l: " + value.l + ", s: " + value.s + ", b: " + value.b + ", g: " + value.g + ", t: " + value.t?.ToString("yyyy-MM-dd'T'HH:mm:ss") + ", k: " + value.k);
+            string logText = "i: " + value.i + ", a: " + value.a + ", o: " + value.o + ", l: " + value.l + ", s: " +
+                 value.s + ", b: " + value.b + ", g: " + value.g + ", t: " +
+                 value.t?.ToString("yyyy-MM-dd'T'HH:mm:ss") + ", k: " + value.k;
             if (SecurityHelper.CheckAccessKey(value))
             {
                 var dtoFactory = new DtoFactory();
                 dtoFactory.InsertOrUpdateDatabase(value);
+            }
+            else
+            {
+                Logger.Warn("Pozice odmítnuta: " + logText);
             }
         }
     }
